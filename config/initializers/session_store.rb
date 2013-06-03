c30eb6437a30e732028290ac218a66658a607c16
Jason Godesky <jefgodesky@gmail.com>
2013-06-02 20:54:14 -0400

    Fixes from Robert Gill

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Overachiever::Application.config.session_store :cookie_store, key: '_overachiever_session'
+Overachiever::Application.config.session_store :cookie_store, :key => '_overachiever_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
