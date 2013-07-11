9e81e65b585154ccc3e314c56e2271ab44380b93
Jason Godesky <jefgodesky@gmail.com>
2013-07-10 21:53:31 -0400

    Changes to Websites
    v1.1

--- app/models/website.rb
+++ app/models/website.rb
@@ -2,5 +2,7 @@ class Website < ActiveRecord::Base
   attr_accessible :domain, :name, :owner
   has_many :achievements
   has_many :awards, :through => :achievements, :uniq => true
-  has_many :records, :through => :achievements
+  has_many :trackers
+  has_many :records, :through => :trackers
+  has_many :triggers, :through => :trackers
 end
